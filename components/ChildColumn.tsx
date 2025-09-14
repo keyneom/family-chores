5dcab05756bf83ea3091ffa9f0a3b02619863d27
Mika <keyneom122@hotmail.com>
2025-09-14 05:42:53 +0000

    Fix build warning

--- components/ChildColumn.tsx
+++ components/ChildColumn.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import TaskItem, { Task } from "./TaskItem";
+import TaskItem from "./TaskItem";
 import { useChoresApp } from "./ChoresAppContext";
 import { useModalControl } from "./ModalControlContext";
 import { shouldChoreRunToday } from "../utils/choreScheduling";
--- docs/_next/static/chunks/7e4b864dcf58aa69.js
+++ docs/_next/static/chunks/7e4b864dcf58aa69.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/8a10ab4121c6a18b.js",
   "static/chunks/f531e5d81db6e01d.js",
   "static/chunks/4540a747d187e8ce.js",
-  "static/chunks/e389508b458f6733.css",
-  "static/chunks/turbopack-5c6686e7898465a3.js"
+  "static/chunks/5b216e075fc1bae0.css",
+  "static/chunks/turbopack-3999690ecded787f.js"
 ])
