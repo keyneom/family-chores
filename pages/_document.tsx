77f7b110280e5f1de07cc51011e71ea4853a7353
keyneom <mika@spire.dev>
2026-03-09 22:21:35 -0500

    Attempting to build again

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'><text y='24' font-size='24'>✓</text></svg>"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
