60ef3428b8613b756de42668d04b5fe88f0af157
keyneom <mika@spire.dev>
2026-04-26 20:48:52 -0500

    minor styling

--- docs/_next/static/chunks/8ef2785b31e18984.js
+++ docs/_next/static/chunks/8ef2785b31e18984.js
@@ -4,6 +4,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/1fe88c78189b9f38.js",
   "static/chunks/6c09ac87d559d5b5.js",
   "static/chunks/2cb2ff8f6012e972.js",
-  "static/chunks/b0c6a90119fc2ad1.css",
-  "static/chunks/turbopack-3fff03c2f7662ade.js"
+  "static/chunks/6a26a80f59b3a421.css",
+  "static/chunks/turbopack-d453147e70d05ab6.js"
 ])
